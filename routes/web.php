6b7b8d0ebdc339bb2947ace9a3e157c6f4a83547
FerlRoque20 <ferl.roquerl2.0@gmail.com>
2024-10-29 14:48:16 -0500

    Se creo el dashboard

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,31 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//Rutas para el administrador
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index')->middleware('auth');
+
+//Rutas para el administrador - Usuarios
+Route::get('/admin/usuarios', [App\Http\Controllers\UsuarioController::class, 'index'])->name('admin.usuarios.index')->middleware('auth');
+
+//Rutas para el administrador - Usuarios - create
+Route::get('/admin/usuarios/create', [App\Http\Controllers\UsuarioController::class, 'create'])->name('admin.usuarios.create')->middleware('auth');
+
+//Rutas para el administrador - Usuarios - store
+Route::post('/admin/usuarios/create', [App\Http\Controllers\UsuarioController::class, 'store'])->name('admin.usuarios.store')->middleware('auth');
+
+//Rutas para el administrador - Usuarios - show
+Route::get('/admin/usuarios/{id}', [App\Http\Controllers\UsuarioController::class, 'show'])->name('admin.usuarios.show')->middleware('auth');
+
+//Rutas para el administrador - Usuarios - edit
+Route::get('/admin/usuarios/{id}/edit', [App\Http\Controllers\UsuarioController::class, 'edit'])->name('admin.usuarios.edit')->middleware('auth');
+
+//Rutas para el administrador - Usuarios - edit - Actualizar Usuario
+Route::put('/admin/usuarios/{id}', [App\Http\Controllers\UsuarioController::class, 'update'])->name('admin.usuarios.update')->middleware('auth');
+
+//Rutas para el administrador - Usuarios - Confirm-delete
+Route::get('/admin/usuarios/{id}/confirm-delete', [App\Http\Controllers\UsuarioController::class, 'confirmDelete'])->name('admin.usuarios.confirmDelete')->middleware('auth');
+
+//Rutas para el administrador - Usuarios - delete
+Route::delete('/admin/usuarios/{id}', [App\Http\Controllers\UsuarioController::class, 'destroy'])->name('admin.usuarios.destroy')->middleware('auth');
+
