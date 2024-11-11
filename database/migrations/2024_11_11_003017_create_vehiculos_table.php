9c5d57b771a4152c8b01e485ac8cb8d14031a82b
FerlRoque20 <ferl.roquerl2.0@gmail.com>
2024-11-10 19:48:39 -0500

    Tabla Vehiculo Completa

---
+++ database/migrations/2024_11_11_003017_create_vehiculos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehiculos', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('marca', 100);
+            $table->string('modelo', 100);
+            $table->string('año', 8);  
+            $table->string('color', 9);  
+            $table->string('placa', 100)->unique(); 
+            $table->string('tipo_conbustible', 100);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehiculos');
+    }
+};
